217bc0851d9a35a151dd3e11e8083bd1a9854167
Christopher Trevino <chrisdadef@gmail.com>
2021-01-16 22:30:36 -0600

    ADDED A FEW THINGS

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 // import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
@@ -30,9 +30,7 @@ export default function Nav() {
           {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton> */}
-          <Typography variant="h6" className={classes.title}>
-            
-          </Typography>
+          <Typography variant="h6" className={classes.title}></Typography>
           {/* <Button color="inherit"></Button> */}
         </Toolbar>
       </AppBar>
